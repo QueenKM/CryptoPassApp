bb4ff7d1777b082e8f179c09db62a404edb28ec4
Kristina Mateva <131942645+QueenKM@users.noreply.github.com>
2026-03-29 19:01:08 +0300

    Add remaining CryptoPass source files

---
+++ CryptoPass/Models/CoreData/UserProfile+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserProfile+CoreDataClass.swift
+//  CryptoPass
+//
+//  Created by Kristina Mateva on 24.12.24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserProfile)
+public class UserProfile: NSManagedObject {
+
+}
