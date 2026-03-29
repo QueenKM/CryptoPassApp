78cdea1435e90bc30f6ba1b7309f49f8610cb786
Kristina Mateva <131942645+QueenKM@users.noreply.github.com>
2026-03-29 19:00:23 +0300

    Add remaining CryptoPass source files

---
+++ CryptoPass/Models/CoreData/Password+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Password+CoreDataClass.swift
+//  CryptoPass
+//
+//  Created by Kristina Mateva on 24.12.24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Password)
+public class Password: NSManagedObject {
+
+}
