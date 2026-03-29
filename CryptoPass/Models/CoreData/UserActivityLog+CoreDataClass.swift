a2b4325ce2da647072ac078368243fe85bd6f63e
Kristina Mateva <131942645+QueenKM@users.noreply.github.com>
2026-03-29 19:00:53 +0300

    Add remaining CryptoPass source files

---
+++ CryptoPass/Models/CoreData/UserActivityLog+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserActivityLog+CoreDataClass.swift
+//  CryptoPass
+//
+//  Created by Kristina Mateva on 24.12.24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserActivityLog)
+public class UserActivityLog: NSManagedObject {
+
+}
